9b823a6742bea933f0fe9f173e509624d04d7a90
ARRW-Core <raig.ramzy@gmail.com>
2022-11-17 12:14:11 +0500

    Layout Start

---
+++ resources/js/Pages/Settings/Ranks.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+</script>
+
+<template>
+    <GuestLayout>
+        A
+    </GuestLayout>
+</template>
+
